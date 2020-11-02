cb0e013157cb0573fbc0fc958f9cbeb999058534
Chander Parkash <30454653+cprajput@users.noreply.github.com>
2020-11-02 15:16:56 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -4,3 +4,14 @@
 
 
 // export class using module.exports
+const Shape = require("Shape");
+
+class Circle extends Shape {
+    constructor(){
+        super();
+        this.calculateArea=()=>{
+            
+        }
+    }
+}
+module.exports = Circle;
